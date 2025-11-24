655b2c72e105d10c8e98a7ca3d649add5f51099e
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-11-24 21:55:46 +0530

    Feat: Initial project setup

---
+++ src/main/java/com/valoriz/BookMgmtSystem/BookMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.valoriz.BookMgmtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valoriz/BookMgmtSystem/BookMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valoriz.BookMgmtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
