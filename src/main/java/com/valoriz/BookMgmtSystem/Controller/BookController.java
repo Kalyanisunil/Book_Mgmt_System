456df2d34b2bdc65b2de1b71743b08c2e4878228
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-11-25 22:04:30 +0530

    Feat: Api endpoints get post and put

--- src/main/java/com/valoriz/BookMgmtSystem/Controller/BookController.java
+++ src/main/java/com/valoriz/BookMgmtSystem/Controller/BookController.java
@@ -1,4 +1,62 @@
 package com.valoriz.BookMgmtSystem.Controller;
 
+
+import com.valoriz.BookMgmtSystem.Models.Book;
+import com.valoriz.BookMgmtSystem.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api")
 public class BookController {
+
+    @Autowired
+    BookRepository bookrepo;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>>getAllBooks(@RequestParam(required = false) String title)
+    {
+        List<Book> books = bookrepo.findAll();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<?> getBookById(@PathVariable("id") String id) {
+        Optional<Book> book = bookrepo.findById(id);
+
+        if (book.isPresent()) {
+            return new ResponseEntity<>(book.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Invalid ID", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> insertBook(@RequestBody Book book) {
+        Book savedBook = bookrepo.save(book);
+
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable("id") String id, @RequestBody Book updatedBook) {
+
+        Optional<Book> bookdata=bookrepo.findById(id);
+
+        if(bookdata.isPresent())
+        {
+            Book book= bookdata.get();
+
+         book.
+
+        }
+    }
 }
