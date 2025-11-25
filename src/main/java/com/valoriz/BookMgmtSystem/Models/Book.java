09c0b98ea6452abbece9bf04631d67b6a259591d
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-11-25 21:22:59 +0530

    Feat:MongoDb connection setup

---
+++ src/main/java/com/valoriz/BookMgmtSystem/Models/Book.java
@@ -0,0 +1,37 @@
+package com.valoriz.BookMgmtSystem.Models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+@Document(collection = "books")
+public class Book {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String title;
+
+    @NotBlank
+    @Size(max = 50)
+    private String author;
+
+    @NotNull
+    private LocalDate publicationDate;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{13}", message = "ISBN must be exactly 13 digits")
+    private String isbn;
+
+    @NotBlank
+    private String genre;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private Integer rating;
+
+}
