fe9656c91a25ddb4059a27c0baa51f91cadffd64
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-12-03 21:28:26 +0530

    Feat: Add link for all books

--- Frontend/bookmgmt/src/Components/NavbarComp.js
+++ Frontend/bookmgmt/src/Components/NavbarComp.js
@@ -5,9 +5,11 @@ export default function NavbarComp() {
     <nav className="navbar navbar-dark bg-dark px-3">
       <Link className="navbar-brand" to="/">Book Management</Link>
 
-      <div>
-        <Link className="btn btn-primary" to="/add">Add Book</Link>
-      </div>
+    <div>
+  <Link className="btn btn-primary me-2" to="/add">Add Book</Link>
+  <Link className="btn btn-secondary" to="/">Back to Book List</Link>
+</div>
+
     </nav>
   );
 }
