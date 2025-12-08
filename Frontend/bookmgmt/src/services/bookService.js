5aa865119395972a4b2e3c9d9952704672150c4e
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-12-08 12:21:52 +0530

    Test commit

--- Frontend/bookmgmt/src/services/bookService.js
+++ Frontend/bookmgmt/src/services/bookService.js
@@ -1,26 +1,8 @@
-import axios from "axios";
-
-// Change this to your Railway URL
-const BASE_URL = "https://bookmgmtsystem-production.up.railway.app/api";
-
-
-export const getBooks = () => axios.get(`${BASE_URL}/books`);
-
-export const getBookById = (id) => axios.get(`${BASE_URL}/books/${id}`);
-
-export const addBook = (book) => axios.post(`${BASE_URL}/books`, book);
-
-export const deleteBook = (id) => axios.delete(`${BASE_URL}/delete/${id}`);
-
-export const getGoogleDetails = (isbn) =>
-  axios.get(`${BASE_URL}/proxy/google?isbn=${isbn}`);
-
-
-
 // import axios from "axios";
 
-// // Use local backend
-// const BASE_URL = "http://localhost:8080/api";
+// // Change this to your Railway URL
+// const BASE_URL = "https://bookmgmtsystem-production.up.railway.app/api";
+
 
 // export const getBooks = () => axios.get(`${BASE_URL}/books`);
 
@@ -31,4 +13,22 @@ export const getGoogleDetails = (isbn) =>
 // export const deleteBook = (id) => axios.delete(`${BASE_URL}/delete/${id}`);
 
 // export const getGoogleDetails = (isbn) =>
-//   axios.get(`${BASE_URL}/google?isbn=${isbn}`);
+//   axios.get(`${BASE_URL}/proxy/google?isbn=${isbn}`);
+
+
+
+import axios from "axios";
+
+// Use local backend
+const BASE_URL = "http://localhost:8080/api";
+
+export const getBooks = () => axios.get(`${BASE_URL}/books`);
+
+export const getBookById = (id) => axios.get(`${BASE_URL}/books/${id}`);
+
+export const addBook = (book) => axios.post(`${BASE_URL}/books`, book);
+
+export const deleteBook = (id) => axios.delete(`${BASE_URL}/delete/${id}`);
+
+export const getGoogleDetails = (isbn) =>
+axios.get(`${BASE_URL}/google?isbn=${isbn}`);
